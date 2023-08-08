d8d2a7a6ddfa499857a0f364f4f84d5cbd536c5a
LeeCH <102038187+LeeChoongHwan@users.noreply.github.com>
2023-08-08 19:23:46 +0900

    [level 0] Title: 배열 자르기, Time: 17.57 ms, Memory: 62.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.kt
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.kt
@@ -2,10 +2,12 @@ class Solution {
     fun solution(numbers: IntArray, num1: Int, num2: Int): IntArray {
         var answer = mutableListOf<Int>()
         
-        for(num in num1..num2) {
-            answer.add(numbers[num])
-        }
+//         for(num in num1..num2) {
+//             answer.add(numbers[num])
+//         }
         
-        return answer.toIntArray()
+//         return answer.toIntArray()
+
+            return numbers.sliceArray(num1..num2)
     }
 }
